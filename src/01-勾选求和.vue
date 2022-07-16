5f576017d33c39db7971002db777c6b817076421
yang <young_yang95@163.com>
2022-07-16 21:43:35 +0800

    学生信息完成,其他作业在day02

---
+++ src/01-勾选求和.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span v-for="(item, index) in arr" :key="index">
+      <input type="checkbox" :value="item" v-model="num"/><span>{{ item }} /</span>
+    </span>
+    <p>您选中的元素，累加和为：{{sum}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      num:[]
+    };
+  },
+  computed:{
+    
+    sum(){
+      console.log(this.num);
+      return this.num.reduce((sum,next) => {
+        return sum += next
+      },0)
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <span>姓名:</span>
+      <input type="text" v-model="Nname" />
+    </div>
+    <div>
+      <span>年龄:</span>
+      <input type="number" v-model="Nage" />
+    </div>
+    <div>
+      <span>性别:</span>
+      <select v-model="Nsex">
+        <option value="男">男</option>
+        <option value="女">女</option>
+      </select>
+    </div>
+    <div>
+      <button @click="add">添加/修改</button>
+    </div>
+    <div>
+      <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>性别</th>
+          <th>操作</th>
+        </tr>
+        <tr v-for="(item, index) in arr" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+          <td>{{ item.sex }}</td>
+          <td>
+            <button :value="item" @click="del(index)">删除</button>
+            <button @click="edi(index)">编辑</button>
+          </td>
+        </tr>
+      </table>
+      <div>{{ Nname }}</div>
+      <div>{{ Nage }}</div>
+      <div>{{ Nsex }}</div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      arr: [
+        {
+          name: "Tom",
+          age: 18,
+          sex: "男",
+        },
+        {
+          name: "Jerry",
+          age: 119,
+          sex: "男",
+        },
+        {
+          name: "Dogge",
+          age: 18,
+          sex: "女",
+        },
+      ],
+      Nname: "",
+      Nage: "",
+      Nsex: "男",
+      Arr1: {},
+      id: 1,
+      status: false,
+    };
+  },
+  methods: {
+    add() {
+      console.log(this.arr);
+      let obj = { name: this.Nname, age: this.Nage, sex: this.Nsex };
+      //  console.log(obj);
+      if (!this.status) {
+        this.arr.push(obj);
+      } else {
+        this.arr.splice(this.id, 1, obj);
+        this.Nname = "";
+        this.Nage = "";
+        this.Nsex = "男";
+      }
+    },
+    del(val) {
+      // console.log(val);
+      this.arr.splice(val, 1);
+    },
+    edi(val) {
+      console.log(this.arr[val]);
+      console.log(this.arr[val].status);
+      this.arr[val].status = 10;
+      this.Nname = this.arr[val].name;
+      this.Nage = this.arr[val].age;
+      this.Nsex = this.arr[val].sex;
+      // this.arr.splice(val, 1)
+      this.id = val;
+      this.status = true;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
